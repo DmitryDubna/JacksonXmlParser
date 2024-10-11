42b6600069df73401e342056d82359bd5459ba9f
Дмитрий С. Телков <telkovds@pba.su>
2024-10-11 11:48:26 +0300

    Добавлен парсинг новых типов

--- src/main/java/com/example/jacksonxmlparser/JacksonXmlParserApplication.java
+++ src/main/java/com/example/jacksonxmlparser/JacksonXmlParserApplication.java
@@ -1,9 +1,11 @@
 package com.example.jacksonxmlparser;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Map;
 
 @SpringBootApplication
@@ -11,9 +13,12 @@ public class JacksonXmlParserApplication {
 
     public static void main(String[] args) {
         try {
+//            String xml = Files.readString(Paths.get("/home/dmitry/work/JavaProjects/JacksonXmlParser/data.xml"));
+
             OdataMetadataXmlParser.OdataMetadata data = OdataMetadataXmlParser.parse(xml);
-            Map<String, String> types = data.toPropertyTypes();
-            System.out.println(types);
+            System.out.println(data);
+            var types = data.toPropertyTypes();
+            System.out.println("PropertyTypes:\n" + types);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -24,9 +29,11 @@ public class JacksonXmlParserApplication {
             <edmx:Edmx Version="4.0" xmlns:edmx="http://docs.oasis-open.org/odata/ns/edmx">
                 <edmx:DataServices>
                     <Schema Namespace="Visary.Audit.Data.Entities" xmlns="http://docs.oasis-open.org/odata/ns/edm">
-                        <EntityType Name="EntityAuditEntry">
+                        <EntityType Name="EntityAuditEntry" OpenType="true">
                             <Key>
-                                <PropertyRef Name="ID"/>
+                                <PropertyRef Name="Recorder"/>
+                                <PropertyRef Name="LineNumber"/>
+                                <PropertyRef Name="Recorder_Type"/>
                             </Key>
                             <Property Name="ID" Type="Edm.Int32" Nullable="false"/>
                             <Property Name="Hidden" Type="Edm.Boolean" Nullable="false"/>
@@ -43,6 +50,18 @@ public class JacksonXmlParserApplication {
                             <Property Name="Source" Type="Edm.String"/>
                             <Property Name="Ip" Type="Edm.String"/>
                             <Property Name="UserAgent" Type="Edm.String"/>
+                            <NavigationProperty Name="ВалютаCr"
+                                    Relationship="StandardODATA.AccountingRegister_Хозрасчетный_RecordType_ВалютаCr"
+                                    FromRole="Begin"
+                                    ToRole="End"/>
+                            <NavigationProperty Name="ПодразделениеDr"
+                                    Relationship="StandardODATA.AccountingRegister_Хозрасчетный_RecordType_ПодразделениеDr"
+                                    FromRole="Begin"
+                                    ToRole="End"/>
+                            <NavigationProperty Name="ПодразделениеCr"
+                                    Relationship="StandardODATA.AccountingRegister_Хозрасчетный_RecordType_ПодразделениеCr"
+                                    FromRole="Begin"
+                                    ToRole="End"/>
                         </EntityType>
                         <EntityType Name="IdentityAuditEntry">
                             <Key>
@@ -60,6 +79,88 @@ public class JacksonXmlParserApplication {
                             <Property Name="Ip" Type="Edm.String"/>
                             <Property Name="UserAgent" Type="Edm.String"/>
                         </EntityType>
+                        <ComplexType Name="AccumulationRegister_ЗакупленныеТоварыКомитентов_Balance">
+                            <Property Name="Организация_Key"
+                                    Type="Edm.Guid"
+                                    Nullable="true"/>
+                            <Property Name="Контрагент_Key"
+                                    Type="Edm.Guid"
+                                    Nullable="true"/>
+                            <Property Name="ДоговорКонтрагента_Key"
+                                    Type="Edm.Guid"
+                                    Nullable="true"/>
+                            <Property Name="Номенклатура_Key"
+                                    Type="Edm.Guid"
+                                    Nullable="true"/>
+                            <Property Name="Партия"
+                                    Type="Edm.String"
+                                    Nullable="true"/>
+                            <Property Name="КоличествоBalance"
+                                    Type="Edm.Double"
+                                    Nullable="true"/>
+                            <Property Name="СуммаBalance"
+                                    Type="Edm.Double"
+                                    Nullable="true"/>
+                            <Property Name="СуммаНДСBalance"
+                                    Type="Edm.Double"
+                                    Nullable="true"/>
+                            <Property Name="Партия_Type"
+                                    Type="Edm.String"
+                                    Nullable="true"/>
+                        </ComplexType>
+                        <ComplexType Name="AccumulationRegister_ВыработкаМатериалов_RowType">
+                            <Property Name="Recorder"
+                                    Type="Edm.String"
+                                    Nullable="false"/>
+                            <Property Name="Period"
+                                    Type="Edm.DateTime"
+                                    Nullable="true"/>
+                            <Property Name="LineNumber"
+                                    Type="Edm.Int64"
+                                    Nullable="false"/>
+                            <Property Name="Active"
+                                    Type="Edm.Boolean"
+                                    Nullable="true"/>
+                            <Property Name="Организация_Key"
+                                    Type="Edm.Guid"
+                                    Nullable="true"/>
+                            <Property Name="Номенклатура_Key"
+                                    Type="Edm.Guid"
+                                    Nullable="true"/>
+                            <Property Name="Количество"
+                                    Type="Edm.Double"
+                                    Nullable="true"/>
+                            <Property Name="Recorder_Type"
+                                    Type="Edm.String"
+                                    Nullable="false"/>
+                        </ComplexType>
+                        <Association Name="AccountingRegister_Хозрасчетный_RecordType_AccountDr">
+                            <End Role="Begin"
+                                    Type="StandardODATA.AccountingRegister_Хозрасчетный_RecordType"
+                                    Multiplicity="*"/>
+                            <End Role="End"
+                                    Type="StandardODATA.ChartOfAccounts_Хозрасчетный"
+                                    Multiplicity="0..1"/>
+                        </Association>
+                        <Association Name="AccountingRegister_Хозрасчетный_RecordType_AccountCr">
+                            <End Role="Begin"
+                                    Type="StandardODATA.AccountingRegister_Хозрасчетный_RecordType"
+                                    Multiplicity="*"/>
+                            <End Role="End"
+                                    Type="StandardODATA.ChartOfAccounts_Хозрасчетный"
+                                    Multiplicity="0..1"/>
+                        </Association>
+                        <EnumType Name="ЧастиЖурналаУчетаСчетовФактур"
+                                UnderlyingType="Edm.Int32">
+                            <Member Name="ВыставленныеСчетаФактуры"/>
+                            <Member Name="ПолученныеСчетаФактуры"/>
+                        </EnumType>
+                        <EnumType Name="ХарактерВыплатыЗарплаты"
+                                UnderlyingType="Edm.Int32">
+                            <Member Name="Аванс"/>
+                            <Member Name="Зарплата"/>
+                            <Member Name="Межрасчет"/>
+                        </EnumType>
                     </Schema>
                     <Schema Namespace="Default" xmlns="http://docs.oasis-open.org/odata/ns/edm">
                         <EntityContainer Name="Container">
--- src/main/java/com/example/jacksonxmlparser/OdataMetadataXmlParser.java
+++ src/main/java/com/example/jacksonxmlparser/OdataMetadataXmlParser.java
@@ -2,6 +2,7 @@ package com.example.jacksonxmlparser;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonRootName;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
@@ -24,17 +25,14 @@ public class OdataMetadataXmlParser {
 
 //        ObjectMapper jsonMapper = new ObjectMapper();
 //        String json = jsonMapper.writeValueAsString(node);
-//
 //        System.out.println(json);
 
         XmlMapper xmlMapper = new XmlMapper();
         OdataMetadata odataMetadata = xmlMapper.readValue(xml, OdataMetadata.class);
-//        System.out.println(odataMetadata);
 
-//        XmlMapper xmlMapper = new XmlMapper();
 //        xmlMapper.enable(SerializationFeature.INDENT_OUTPUT);
-//        var json = xmlMapper.writeValueAsString(new OdataMetadata());
-//        System.out.println(json);
+//        var json = xmlMapper.writeValueAsString(odataMetadata);
+//        System.out.println("json:\n" + json);
 
         return odataMetadata;
     }
@@ -46,11 +44,11 @@ public class OdataMetadataXmlParser {
     @JsonRootName("Edmx")
     static class OdataMetadata {
         @JacksonXmlProperty(localName = "Version", isAttribute = true)
-        private String version/* = "1.0"*/;
+        private String version;
 
         @JacksonXmlElementWrapper(localName = "DataServices")
         @JacksonXmlProperty(localName = "Schema")
-        private List<Schema> dataServices/* = List.of(new Schema())*/;
+        private List<Schema> dataServices;
 
         public Map<String, String> toPropertyTypes()
         {
@@ -60,6 +58,7 @@ public class OdataMetadataXmlParser {
                     .filter(entityType -> Objects.nonNull(entityType.getProperties()))
                     .flatMap(entityType -> entityType.getProperties().stream())
                     .distinct()
+                    .peek(System.out::println)
                     .collect(Collectors.toMap(Property::getName, Property::getType));
         }
     }
@@ -69,15 +68,27 @@ public class OdataMetadataXmlParser {
     @JsonIgnoreProperties(ignoreUnknown = true)
     static class Schema {
         @JacksonXmlProperty(localName = "Namespace", isAttribute = true)
-        private String namespace/* = "my-namespace"*/;
+        private String namespace;
 
         @JacksonXmlElementWrapper(useWrapping = false)
         @JacksonXmlProperty(localName = "EntityType")
-        private List<EntityType> entityTypes/* = List.of(new EntityType())*/;
+        private List<EntityType> entityTypes;
+
+        @JacksonXmlElementWrapper(useWrapping = false)
+        @JacksonXmlProperty(localName = "ComplexType")
+        private List<ComplexType> complexTypes;
+
+        @JacksonXmlElementWrapper(useWrapping = false)
+        @JacksonXmlProperty(localName = "Association")
+        private List<Association> associations;
+
+        @JacksonXmlElementWrapper(useWrapping = false)
+        @JacksonXmlProperty(localName = "EnumType")
+        private List<EnumType> enumTypes;
 
         @JacksonXmlElementWrapper(localName = "EntityContainer")
         @JacksonXmlProperty(localName = "EntitySet")
-        private List<EntitySet> entityContainer/* = List.of(new EntitySet())*/;
+        private List<EntitySet> entityContainer;
     }
 
     @Getter
@@ -85,42 +96,77 @@ public class OdataMetadataXmlParser {
     @JsonIgnoreProperties(ignoreUnknown = true)
     static class EntityType {
         @JacksonXmlProperty(localName = "Name", isAttribute = true)
-        private String name/* = "my-name"*/;
+        private String name;
+
+        @JacksonXmlProperty(localName = "OpenType", isAttribute = true)
+        private boolean openType;
 
         @JacksonXmlProperty(localName = "Key")
-        private Key key/* = new Key()*/;
+        private Key key;
 
         @JacksonXmlElementWrapper(useWrapping = false)
         @JacksonXmlProperty(localName = "Property")
-        private List<Property> properties/* = List.of(new Property())*/;
+        private List<Property> properties;
+
+        @JacksonXmlElementWrapper(useWrapping = false)
+        @JacksonXmlProperty(localName = "NavigationProperty")
+        private List<NavigationProperty> navigationProperties;
+    }
+
+    @Getter
+    @ToString
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    static class ComplexType {
+        @JacksonXmlProperty(localName = "Name", isAttribute = true)
+        private String name;
+
+        @JacksonXmlElementWrapper(useWrapping = false)
+        @JacksonXmlProperty(localName = "Property")
+        private List<Property> properties;
+    }
+
+    @Getter
+    @ToString
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    static class Association {
+        @JacksonXmlProperty(localName = "Name", isAttribute = true)
+        private String name;
+
+        @JacksonXmlElementWrapper(useWrapping = false)
+        @JacksonXmlProperty(localName = "End")
+        private List<End> ends;
+    }
+
+    @Getter
+    @ToString
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    static class EnumType {
+        @JacksonXmlProperty(localName = "Name", isAttribute = true)
+        private String name;
+
+        @JacksonXmlProperty(localName = "UnderlyingType", isAttribute = true)
+        private String underlyingType;
+
+        @JacksonXmlElementWrapper(useWrapping = false)
+        @JacksonXmlProperty(localName = "Member")
+        private List<Member> members;
     }
 
     @Getter
     @ToString
     @JsonIgnoreProperties(ignoreUnknown = true)
     static class Key {
-        @JacksonXmlProperty(localName = "Name", isAttribute = true)
-        private String name/* = "key-name"*/;
-
+        @JacksonXmlElementWrapper(useWrapping = false)
         @JacksonXmlProperty(localName = "PropertyRef")
-        private PropertyRef propertyRef/* = new PropertyRef()*/;
+        private List<PropertyRef> propertyRefs;
     }
-
-//    @Getter
-//    @ToString
-//    @EqualsAndHashCode
-//    @JsonIgnoreProperties(ignoreUnknown = true)
-//    static class NamedItem {
-//        @JacksonXmlProperty(localName = "Name", isAttribute = true)
-//        private String name;
-//    }
 
     @Getter
     @ToString
     @JsonIgnoreProperties(ignoreUnknown = true)
     static class PropertyRef {
         @JacksonXmlProperty(localName = "Name", isAttribute = true)
-        private String name/* = "ID"*/;
+        private String name;
     }
 
     @Getter
@@ -129,23 +175,62 @@ public class OdataMetadataXmlParser {
     @JsonIgnoreProperties(ignoreUnknown = true)
     static class Property {
         @JacksonXmlProperty(localName = "Name", isAttribute = true)
-        private String name/* = "ID"*/;
+        private String name;
 
         @JacksonXmlProperty(localName = "Type", isAttribute = true)
-        private String type/* = "Edm.Guid"*/;
+        private String type;
 
         @JacksonXmlProperty(localName = "Nullable", isAttribute = true)
-        private boolean isNullable/* = true*/;
+        private boolean isNullable;
+    }
+
+    @Getter
+    @ToString()
+    @EqualsAndHashCode()
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    static class NavigationProperty {
+        @JacksonXmlProperty(localName = "Name", isAttribute = true)
+        private String name;
+
+        @JacksonXmlProperty(localName = "Relationship", isAttribute = true)
+        private String relationship;
+
+        @JacksonXmlProperty(localName = "FromRole", isAttribute = true)
+        private String fromRole;
+
+        @JacksonXmlProperty(localName = "ToRole", isAttribute = true)
+        private String toRole;
+    }
+
+    @Getter
+    @ToString()
+    @EqualsAndHashCode()
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    static class End {
+        @JacksonXmlProperty(localName = "Role", isAttribute = true)
+        private String role;
+
+        @JacksonXmlProperty(localName = "Type", isAttribute = true)
+        private String type;
+
+        @JacksonXmlProperty(localName = "Multiplicity", isAttribute = true)
+        private String multiplicity;
+    }
+
+
+    @ToString(callSuper = true)
+    static class Member extends PropertyRef {
     }
 
     @Getter
     @ToString
+    @EqualsAndHashCode()
     @JsonIgnoreProperties(ignoreUnknown = true)
     static class EntitySet {
         @JacksonXmlProperty(localName = "Name", isAttribute = true)
-        private String name/* = "Audit"*/;
+        private String name;
 
         @JacksonXmlProperty(localName = "EntityType", isAttribute = true)
-        private String entityType/* = "Visary.Audit.Data.Entities.EntityAuditEntry"*/;
+        private String entityType;
     }
 }
